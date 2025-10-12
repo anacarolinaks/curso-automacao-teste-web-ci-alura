797ede583f2f125361d06f69129a60b628b9d8e7
Ana Carolina <anacarolinaks@gmail.com>
2025-10-12 14:46:40 -0300

    Finalizado Modulo 3

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,4 +1,4 @@
-/* global cy, Cypress, */
+/* global cy, Cypress, expect */
 
 Cypress.Commands.add('login', (email, senha) => {
     cy.session([email, senha], () => {
@@ -26,5 +26,22 @@ Cypress.Commands.add('cadastraEspecialista', (nome, email, senha, especialidade,
     cy.get('[data-test="inputEspecialistaNumero"]').type(numero)
     cy.get('[data-test="inputEspecialistaComplemento"]').type(complemento)
     cy.get('[data-test="inputEspecialistaEstado"]').type(estado)
+})
 
+Cypress.Commands.add('loginApi', (email, senha) => {
+    
+    cy.request({
+        method: 'POST',
+        url: Cypress.env('api_login'),
+        body: {
+            email: email,
+            senha: senha
+        }                               
+    }).then((response) => {
+        expect(response.status).to.eq(200);
+        expect(response.body.auth).to.be.true;
+        expect(response.body.rota).to.eq('/clinica');
+        expect(response.body.token).to.exist;
+        cy.wrap(response.body.token).as('token');
+    })  
 })
